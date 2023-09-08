7c8e24e5dcfa0169c2cf776e1d2a91b754c45a83
Deepak Gurjar <78991392+deepak-gurjar07@users.noreply.github.com>
2023-09-08 14:36:12 +0530

    Add files via upload

---
+++ Arrays/Initialization.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    // intialization, input and ouput of 1D array
+/*
+    int size;
+    cout<<"Enter the size of array: ";
+    cin>>size;
+    int  arr[size] ;
+    cout<<"Enter the elements of array:"<<endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"The elements of array:"<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    */
+    
+     // intialization, input and ouput of 2D array
+
+    
+
+    return 0;
+}
